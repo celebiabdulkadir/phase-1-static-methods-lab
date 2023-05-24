02521291aef56f705ab0120b8fb7b9d0151075e3
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-24 11:00:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
-  //add static methods here
+	//add static methods here
+
+	static capitalize(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+	static sanitize(string) {
+		return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+	}
+	static titleize(string) {
+		let words = string.split(' ');
+		let title = [];
+		let exceptions = [
+			'the',
+			'a',
+			'an',
+			'but',
+			'of',
+			'and',
+			'for',
+			'at',
+			'by',
+			'from',
+		];
+		for (let i = 0; i < words.length; i++) {
+			if (i === 0) {
+				title.push(this.capitalize(words[i]));
+			} else {
+				if (exceptions.includes(words[i])) {
+					title.push(words[i]);
+				} else {
+					title.push(this.capitalize(words[i]));
+				}
+			}
+		}
+		return title.join(' ');
+	}
 }
